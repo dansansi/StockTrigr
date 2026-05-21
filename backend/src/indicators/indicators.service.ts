e07bff7930198396115416d7e5f2fb728d0c933b
Danilo Santos <dansansi@hotmail.com>
2026-05-21 13:22:25 -0300

    BB calcule ok.

---
+++ backend/src/indicators/indicators.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+
+export interface BandResult {
+  sma: number;
+  upper: number;
+  lower: number;
+}
+
+@Injectable()
+export class IndicatorService {
+  calculateBB(prices: number[], deviation: number): BandResult {
+    const sma = prices.reduce((sum, p) => sum + p, 0) / prices.length;
+    const variance = prices.reduce((sum, p) => sum + Math.pow(p - sma, 2), 0) / prices.length;
+    const std = Math.sqrt(variance);
+
+    return {
+      sma,
+      upper: sma + deviation * std,
+      lower: sma - deviation * std,
+    };
+  }
+}
