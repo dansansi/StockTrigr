e09792d57be07919d1b2a68b9697ebdeaf761095
Danilo Santos <dansansi@hotmail.com>
2026-05-27 18:19:22 -0300

    JobsModule created.

---
+++ backend/src/jobs/jobs.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { IntradayScanJob } from './intraday-scan.job';
+import { DailyCloseJob } from './daily-close.job';
+import { MarketDataModule } from 'src/market-data/market-data.module';
+import { DailyPriceModule } from 'src/daily-price/daily-price.module';
+import { IndicatorModule } from 'src/indicators/indicators.module';
+import { WatchlistModule } from 'src/watchlist/watchlist.module';
+import { AlertModule } from 'src/alert/alert.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    MarketDataModule,
+    WatchlistModule,
+    DailyPriceModule,
+    IndicatorModule,
+    AlertModule,
+    PrismaModule,
+  ],
+  providers: [IntradayScanJob, DailyCloseJob],
+})
+export class JobsModule {}
